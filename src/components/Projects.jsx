806ee3ab8de62eeac8622dfb97dc1e85aad270cc
Meeth Patel <meeth2060@gmail.com>
2025-07-07 20:57:18 +0530

    ,,

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ function Projects() {
             <div className={`grid md:grid-cols-2 gap-8 ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}>
                  <a href="https://github.com/meethp20/docuchat-app" target='_blank' className={`block p-6 ${theme === 'dark' ? 'bg-[#120B1D] border-purple-900' : 'bg-gray-100 border-gray-200'} border rounded-lg shadow hover:bg-opacity-80 hover:scale-105 transition-all duration-300`}>
                     <h5 className={`mb-2 text-2xl font-bold tracking-tight ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}>Docuchat-app</h5>
-                    <p className={`font-normal ${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>
+                    <p className={`font-normal ${theme === 'dark' ? 'text-gray-300' : 'text-gray-700'}`}>
   DocuChat is a modern web application that combines PDF document processing with AI-powered chat capabilities.</p>
                     <div className="mt-4 flex-wrap gap-2">
                         <span className="px-3 py-1 text-sm bg-purple-900 text-purple-200 rounded whitespace-nowrap">Next.js</span>
